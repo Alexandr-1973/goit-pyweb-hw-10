928f02a1505c89b327a30ce15488069f9452b131
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-03-18 21:29:49 +0200

    start

---
+++ hw10_project/quotes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .utils import get_mongodb
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def main(request, page=1):
+    db = get_mongodb()
+
+    quotes = list(db.quotes.find())
+    authors = list(db.authors.find())
+    authors_dict = {author["_id"]: author["fullname"] for author in authors}
+    for quote in quotes:
+        quote["author"] = authors_dict.get(quote["author"])
+    per_page=10
+    paginator=Paginator(quotes, per_page)
+    quotes_on_page=paginator.page(page)
+    return render(request, "quotes/index.html", context={"quotes":quotes_on_page})
